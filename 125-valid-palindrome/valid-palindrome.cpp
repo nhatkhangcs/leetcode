5a85a4910933f6f44e008257849b080246d8b1f6
Vo Hoang Nhat Khang (Chris) <khang.vo872003@hcmut.edu.vn>
2023-09-04 21:30:14 +0700

    Time: 8 ms (33.07%) | Memory: 7.4 MB (60.30%) - LeetSync

---
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool isPalindrome(string s) {
+        int i = 0, j = s.size() - 1;
+        while(i < j){
+            if(!isalnum(s[i])) i++;
+            else if(!isalnum(s[j])) j--;
+            else if(tolower(s[i++]) != tolower(s[j--])) return false;
+        }
+        return true;
+    }
+};
