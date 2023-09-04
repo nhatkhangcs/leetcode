394f2af877f97df016543bbd0adaf7d278218b81
Vo Hoang Nhat Khang (Chris) <khang.vo872003@hcmut.edu.vn>
2023-09-04 21:34:33 +0700

    Time: 78 ms (46.21%) | Memory: 59.1 MB (30.56%) - LeetSync

---
+++ 11-container-with-most-water/container-with-most-water.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int maxArea(vector<int>& height) {  // O(n)
+        int l = 0, r = height.size() - 1;
+        int res = 0;
+        while (l < r) {
+            res = max(res, min(height[l], height[r]) * (r - l));
+            if (height[l] < height[r]) ++l;
+            else --r;
+        }
+        return res;
+    }
+};
