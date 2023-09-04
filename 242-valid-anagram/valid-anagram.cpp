d38eb83cf3529665b3420fe1c4edc1be44d817cb
Vo Hoang Nhat Khang (Chris) <khang.vo872003@hcmut.edu.vn>
2023-09-04 21:30:40 +0700

    Time: 7 ms (78.53%) | Memory: 7.4 MB (19.61%) - LeetSync

---
+++ 242-valid-anagram/valid-anagram.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        if(s.size() != t.size()) return false;
+        vector<int> count(26, 0);
+        for(int i = 0; i < s.size(); i++){
+            count[s[i] - 'a']++;
+            count[t[i] - 'a']--;
+        }
+        for(int i = 0; i < 26; i++){
+            if(count[i] != 0) return false;
+        }
+        return true;
+    }
+};
