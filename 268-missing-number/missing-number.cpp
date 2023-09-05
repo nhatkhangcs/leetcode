3743e3084a6f434115cfab71c94ce0c43bdee7e7
Vo Hoang Nhat Khang (Chris) <khang.vo872003@hcmut.edu.vn>
2023-09-05 22:54:04 +0700

    Time: 17 ms (51.60%) | Memory: 18 MB (45.24%) - LeetSync

---
+++ 268-missing-number/missing-number.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        sort(nums.begin(), nums.end());
+        if(nums[0] != 0) return 0;
+        for (int i = 0; i < nums.size() - 1; i++){
+            if (nums[i] != nums[i + 1] - 1) return nums[i] + 1;
+        }
+
+        return nums[nums.size() - 1] + 1;
+    }
+};
